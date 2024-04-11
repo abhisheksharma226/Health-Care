d5543a84c42d8445a3b49d566dfbda6cc602c6d4
Abhishek Sharma <avisheksharma2004@gmail.com>
2024-04-11 17:45:54 +0530

    Home page done

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
+
+//for Css
+app.use(express.static(path.join(__dirname, "public")));
+
+//for Ejs
+app.set("view engine" , "ejs");
+app.set("views" , path.resolve("./views"));
+
+
+app.get("/" , (req , res) => {
+    res.render("home");
+})
 
 app.listen(PORT  , ()=> {
     console.log(`Server Started at ${PORT}`);
